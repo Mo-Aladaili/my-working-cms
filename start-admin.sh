7af7ba1a68378a89bd9a6ce2dba9e776b49c61a9
Mo_Aladaili <84664161+Mo-Aladaili@users.noreply.github.com>
2026-07-02 03:42:03 +0300

    Update start-admin.sh

--- start-admin.sh
+++ start-admin.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 set -e
 
+echo "START-ADMIN SCRIPT IS RUNNING"
+
 mkdir -p /usr/local/etc
 
 cat > /usr/local/etc/cms.toml <<EOF
 database = """$DATABASE_URL"""
 EOF
+
+echo "CMS TOML CREATED:"
+ls -l /usr/local/etc/cms.toml
+cat /usr/local/etc/cms.toml | sed 's/:.*@/:PASSWORD@/g'
 
 cmsInitDB || true
 
