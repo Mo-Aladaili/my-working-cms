4d06c425115be3bcb3261318c5f928a660cdef1c
Mo_Aladaili <84664161+Mo-Aladaili@users.noreply.github.com>
2026-07-02 03:41:48 +0300

    Enhance start-contest.sh with logging and file checks
    Added logging for script execution and CMS TOML creation.

--- start-contest.sh
+++ start-contest.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -e
 
+echo "START-CONTEST SCRIPT IS RUNNING"
+
 mkdir -p /usr/local/etc
 
 cat > /usr/local/etc/cms.toml <<EOF
 database = """$DATABASE_URL"""
 EOF
+
+echo "CMS TOML CREATED:"
+ls -l /usr/local/etc/cms.toml
+cat /usr/local/etc/cms.toml | sed 's/:.*@/:PASSWORD@/g'
 
 cmsContestWebServer --port ${PORT:-10000}
